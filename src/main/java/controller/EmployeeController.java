2a9a2be6e9b4bbc42544ab77029bf0d8bd81e104
pidan <15652786161@163.com>
2019-06-07 18:25:25 +0800

    为了我的部署方便

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -31,7 +31,7 @@ import face.search.FaceSearch;
 import service.EmployeeService;
 
 //部署到服务器上的时候请一定使用 @CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true") 才能和前端正常交互
-@CrossOrigin(origins = "*", maxAge = 3600,allowCredentials="true")
+@CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
