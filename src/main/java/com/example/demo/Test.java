122ce11cfd651f2701cf1792145d709623b37dad
pidan <15652786161@163.com>
2019-06-07 16:58:07 +0800

    Tets

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) { 
+		List<String> ans=new ArrayList<String>();
+		ans.add("123");
+		ans.add("456");
+		ans.add("789");
+		System.out.println(ans.toString());
+	}
+}
