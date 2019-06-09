0d0b9b85c9fa3212e6c6de74f1dddbedeb1608a8
pidanself <15652786161@163.com>
2019-06-09 18:27:21 +0800

    merge2

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -35,11 +35,7 @@ import face.search.FaceSearch;
 import service.EmployeeService;
 
 //部署到服务器上的时候请一定使用 @CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true") 才能和前端正常交互
-<<<<<<< HEAD
-@CrossOrigin(origins = {"http://39.105.38.34","172.22.245.211"}, maxAge = 3600,allowCredentials="true")
-=======
 @CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
->>>>>>> master
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
--- src/main/java/controller/ErrorController.java
+++ src/main/java/controller/ErrorController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 //部署到服务器上的时候请一定使用 @CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true") 才能和前端正常交互
 @CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
->>>>>>> master
 @RestController
 @RequestMapping("/error")
 public class ErrorController {
