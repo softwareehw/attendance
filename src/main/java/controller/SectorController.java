cb8cef9e366fba8c09df40bb972d6bc7d581fa79
enniefu <enniefu@gmail.com>
2019-06-10 11:14:01 +0800

    sector100%

--- src/main/java/controller/SectorController.java
+++ src/main/java/controller/SectorController.java
@@ -3,14 +3,16 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import bean.Sector;
 import service.SectorService;
 
 //部署到服务器上的时候请一定使用 @CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true") 才能和前端正常交互
-@CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
+//@CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
 @RestController
 @RequestMapping("/api/v1/sectors")
 public class SectorController {
@@ -18,15 +20,55 @@ public class SectorController {
 	@Autowired
 	SectorService sectorService;
 	
+	
+	/**
+	 * 获取所有的部门信息
+	 * @return
+	 */
 	@RequestMapping(value="",method=RequestMethod.GET)
 	public String LookForAllSectors(){
 		
 		return sectorService.findAllSector();
 	}
 	
+	/**
+	 * 获取特定的部门信息
+	 * @param sectorId
+	 * @return
+	 */
 	@RequestMapping(value="/{sectorId}",method=RequestMethod.GET)
     public String findSpecificSector(@PathVariable int sectorId){
 		
 		return sectorService.findSectorById(sectorId);
+	}
+	
+	/**
+	 * 添加一个sector
+	 * @param s
+	 * @return
+	 */
+	@RequestMapping(value="",method=RequestMethod.POST)
+    public String AddSector(@RequestBody Sector s){
+		
+		return sectorService.AddSector(s);
+	}
+	
+	
+	/**
+	 * 修改一个sector
+	 * @param sectorId
+	 * @param s
+	 * @return
+	 */
+	@RequestMapping(value="/{sectorId}",method=RequestMethod.PUT)
+    public String ModifySector(@RequestBody Sector s){
+		
+		return sectorService.ModifySector(s);
+	}
+	
+	@RequestMapping(value="/{sectorId}",method=RequestMethod.DELETE)
+    public String deleteSector(@PathVariable int sectorId){
+		
+		return sectorService.DeleteSector(sectorId);
 	}
 }
