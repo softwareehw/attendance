d00a2fde7616d13227e4abb22e91c9d87e31e8e3
enniefu <enniefu@gmail.com>
2019-06-11 15:19:20 +0800

    调整了数据库结构

--- src/main/java/bean/ApplicationForEW.java
+++ src/main/java/bean/ApplicationForEW.java
@@ -4,41 +4,79 @@ import java.util.Date;
 //加班申请
 public class ApplicationForEW {
 	
-	private int applicatedId; //申请单号
-	private int applicatedPerson;
-	private Date date;
-	private int state; //0待审，1为批准，2为驳回
-	private int sectorId;
+	private int applicatedId;
+	
+	private int ewState;
+	private int ewId;
+	private int ratifyId;
+	
+	private Date morningStartTime;
+	private Date morningEndTime;
+	
+	private Date afternoonStartTime;
+	private Date afternoonEndTime;
+	
+	private Date eveningStartTime;
+	private Date eveningEndTime;
 	public int getApplicatedId() {
 		return applicatedId;
 	}
-	public void setApplicatedId(int i) {
-		this.applicatedId = i;
+	public void setApplicatedId(int applicatedId) {
+		this.applicatedId = applicatedId;
 	}
-
-	public Date getDate() {
-		return date;
+	public int getEwState() {
+		return ewState;
 	}
-	public void setDate(Date date) {
-		this.date = date;
+	public void setEwState(int ewState) {
+		this.ewState = ewState;
 	}
-	public int getSectorId() {
-		return sectorId;
+	public int getEwId() {
+		return ewId;
 	}
-	public void setSectorId(int sectorId) {
-		this.sectorId = sectorId;
+	public void setEwId(int ewId) {
+		this.ewId = ewId;
 	}
-	public int getState() {
-		return state;
+	public int getRatifyId() {
+		return ratifyId;
 	}
-	public void setState(int state) {
-		this.state = state;
+	public void setRatifyId(int ratifyId) {
+		this.ratifyId = ratifyId;
 	}
-	public int getApplicatedPerson() {
-		return applicatedPerson;
+	public Date getMorningStartTime() {
+		return morningStartTime;
 	}
-	public void setApplicatedPerson(int applicatedPerson) {
-		this.applicatedPerson = applicatedPerson;
+	public void setMorningStartTime(Date morningStartTime) {
+		this.morningStartTime = morningStartTime;
+	}
+	public Date getMorningEndTime() {
+		return morningEndTime;
+	}
+	public void setMorningEndTime(Date morningEndTime) {
+		this.morningEndTime = morningEndTime;
+	}
+	public Date getAfternoonStartTime() {
+		return afternoonStartTime;
+	}
+	public void setAfternoonStartTime(Date afternoonStartTime) {
+		this.afternoonStartTime = afternoonStartTime;
+	}
+	public Date getAfternoonEndTime() {
+		return afternoonEndTime;
+	}
+	public void setAfternoonEndTime(Date afternoonEndTime) {
+		this.afternoonEndTime = afternoonEndTime;
+	}
+	public Date getEveningStartTime() {
+		return eveningStartTime;
+	}
+	public void setEveningStartTime(Date eveningStartTime) {
+		this.eveningStartTime = eveningStartTime;
+	}
+	public Date getEveningEndTime() {
+		return eveningEndTime;
+	}
+	public void setEveningEndTime(Date eveningEndTime) {
+		this.eveningEndTime = eveningEndTime;
 	}
 	
 	
