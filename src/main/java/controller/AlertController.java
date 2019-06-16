4f7475f49d91a6ecd28b71fc3d7c9976ad0b4af8
dxftian <2922699274@qq.com>
2019-06-16 19:09:25 +0800

    调bug

--- src/main/java/controller/AlertController.java
+++ src/main/java/controller/AlertController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import service.AlertService;
 
-@CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
+//@CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
 @RestController
 @RequestMapping("/api/v1/alert")
 public class AlertController {
@@ -20,5 +19,6 @@ public class AlertController {
 	@GetMapping("/find/{employeeId}")
 	public String findAlert(@PathVariable int employeeId) {
 		return alertService.findAlert(employeeId);
+		
 	}
 }
--- src/main/java/serviceimpl/AlertServiceImpl.java
+++ src/main/java/serviceimpl/AlertServiceImpl.java
@@ -1,9 +1,12 @@
 package serviceimpl;
 
+import java.util.List;
+
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import bean.Alert;
 import dao.AlertDao;
 import service.AlertService;
 
@@ -17,7 +20,12 @@ public class AlertServiceImpl implements AlertService {
 	public String findAlert(int employeeId) {
 		// TODO Auto-generated method stub
 		
-		JSONArray jay = new JSONArray(alertDao.findAlert(employeeId));
+		List<Alert> list = alertDao.findAlert(employeeId);
+		int a[] = new int[list.size()];
+		for(int i = 0;i<list.size();i++) {
+			a[i] = list.get(i).getState();
+		}
+		JSONArray jay = new JSONArray(a);
 		alertDao.deleteAlert(employeeId);
 		return jay.toString();
 	}
