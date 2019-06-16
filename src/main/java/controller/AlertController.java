9ca513dfa2858184aa580fe6e3322297be86358c
pidanself <15652786161@163.com>
2019-06-16 19:55:59 +0800

    das

--- src/main/java/controller/AlertController.java
+++ src/main/java/controller/AlertController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import service.AlertService;
 
-//@CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
+@CrossOrigin(origins = "http://39.105.38.34", maxAge = 3600,allowCredentials="true")
 @RestController
 @RequestMapping("/api/v1/alert")
 public class AlertController {
